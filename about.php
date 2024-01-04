656d287949b5715ae2cb35a09a77100b07f4d655
Al-Qadasi1 <mohammed771141936@gmail.com>
2024-01-05 00:15:35 +0800

    alqadasi
    style abut.php

--- about.php
+++ about.php
@@ -2,58 +2,147 @@
 <?php include('includes/navbar.php'); ?>
 <?php include('includes/banner.php'); ?>
 
-<section class="section bg-light">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h4 class="main-heading">About Us</h4>
-                <div class="underline"></div>
-                <p>
-                "Discover the perfect blend of convenience and efficiency with our platform, where you can seamlessly reserve university halls and facilities. Elevate your experience by exploring our user-friendly website designed to make your venue bookings effortlessly smooth. Unlock a world of possibilities for your events and gatherings with just a click away!"
-                </p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
 
-                <p>
-                "Unlocking the Power of Web Development: Group 13's Journey
+   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+   
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
-                In the dynamic realm of web development, Group 13 stands out as a beacon of innovation and expertise. As a dedicated web development group, our passion for crafting seamless online experiences propels us forward. From brainstorming sessions to coding marathons, our collaborative spirit fuels our success.
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="assets/css/style.css">
 
-                Our commitment to excellence reflects in the myriad of projects we undertake, each a testament to our proficiency in turning ideas into digital reality. Whether it's designing interactive interfaces or optimizing backend functionalities, Group 13 is your go-to partner for all things web development.
+</head>
+<body>
+   
+<section class="about">
 
-             Navigating the ever-evolving landscape of technology, our group embraces the latest trends and tools. We stay ahead of the curve, ensuring that our clients receive cutting-edge solutions that resonate with the fast-paced digital world.
+   <div class="row">
 
-              As Group 13, we take pride in our unique approach to web development. We understand that each project is a canvas waiting to be painted with creativity and precision. Our attention to detail, coupled with a client-centric mindset, ensures that we not only meet but exceed expectations.
+      <div class="image">
+         <img src="images/about-img.svg" alt="">
+      </div>
 
-             Visit our website to witness the diverse range of projects we've brought to life. From e-commerce platforms to dynamic web applications, Group 13 leaves an indelible mark on the digital landscape.
+      <div class="content">
+         <h3>why choose us?</h3>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam veritatis minus et similique doloribus? Harum molestias tenetur eaque illum quas? Obcaecati nulla in itaque modi magnam ipsa molestiae ullam consequuntur.</p>
+         <a href="contact.php" class="btn">contact us</a>
+      </div>
 
-             Join us in shaping the future of web development. Let Group 13 be your trusted partner in transforming ideas into immersive online experiences. Explore the possibilities with us and witness the magic of web development unfold before your eyes." </div>
-        </div>    
-    </div>
+   </div>
+
 </section>
 
-<section class="section ">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h4 class="main-heading">Our Services</h4>
-                <div class="underline"></div>
-                <p>
-                "Empower Your Digital Presence with Our Services At Facilities Booking Site, we redefine excellence in [industry/service]. Our commitment to innovation and client satisfaction sets us apart. With a team of dedicated experts, we deliver tailored solutions that propel your business forward.</p>
+<section class="reviews">
+   
+   <h1 class="heading">client's reviews</h1>
 
-                <p>Why choose Facilities Booking Site? </p>
+   <div class="swiper reviews-slider">
 
-                <p>1. Strategic Expertise: Our seasoned professionals bring a wealth of strategic insight to every project, ensuring a holistic approach to your business needs.</p>
+   <div class="swiper-wrapper">
 
-                <p>2. Cutting-edge Technology: Stay ahead in the digital landscape with our utilization of the latest technologies. We embrace innovation to provide you with solutions that stand the test of time.</p>
+      <div class="swiper-slide slide">
+         <img src="images/1.jpg" alt="">
+         <p></p>
+         <p>Major: BIW </p>
+         <p>Semster : 5 </p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Mohammed Taresh</h3>
+      </div>
 
-                <p>3. Client-Centric Approach: Your success is our priority. We collaborate closely with clients, understanding their goals and delivering results that exceed expectations.</p>
+      <div class="swiper-slide slide">
+         <img src="images/2.jpg" alt="">
+         <p></p>
+         <p>Major: BIM </p>
+         <p>Semster : 5 </p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Ali Elhefnawy</h3>
+      </div>
 
-                <p>4. Custom Solutions: No two businesses are alike. We tailor our services to fit your unique requirements, offering personalized solutions that align with your vision.</p>
+      <div class="swiper-slide slide">
+         <img src="images/3.jpg" alt="">
+         <p></p>
+         <p>Major: BIM </p>
+         <p>Semster : 5 </p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Nur Zahrah</h3>
+      </div>
 
-                <p>Explore the world of possibilities with [Your Company Name]. Elevate your digital presence and unlock new opportunities. Let's embark on a journey of success together."</p>
+      <div class="swiper-slide slide">
+         <img src="images/4.jpg" alt="">
+         <p> </p>
+         <p>Major: BIM</p>
+         <p>Semster : 5 </p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Nur Syafiqah</h3>
+      </div>
 
-            </div>
-        </div>    
-    </div>
+      <div class="swiper-slide slide">
+         <img src="images/5.jpg" alt="">
+         <p></p>
+         <p>Major: BIM </p>
+         <p>Semster : 5 </p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Chiew Mei Chun</h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/6.jpg" alt="">
+         <p> </p>
+         <p>Major : BIM </p>
+         <p>Semster : 5 </p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Yuki Kot</h3>
+      </div>
+
+   </div>
+
+   <div class="swiper-pagination"></div>
+
+   </div>
+
 </section>
 
 
@@ -62,4 +151,60 @@
 
 
 
+
+
+
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+<script src="js/script.js"></script>
+
+<script>
+
+var swiper = new Swiper(".reviews-slider", {
+   loop:true,
+   spaceBetween: 20,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+   },
+   breakpoints: {
+      0: {
+        slidesPerView:1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+   },
+});
+
+</script>
 <?php include('includes/footer.php'); ?>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
